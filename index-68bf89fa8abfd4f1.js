0ca88d4d7a5bb3ec8ca54bbb5ae28e835ceaff78
as-is-prog <as-is-prog@users.noreply.github.com>
2022-04-10 05:27:47 +0000

    deploy: 3bfd84e0a14d2ccb93b50df578315f1415f49500

--- index-68bf89fa8abfd4f1.js
+++ index-68bf89fa8abfd4f1.js
@@ -274,7 +274,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-8f7912509d70f337_bg.wasm', import.meta.url);
+        input = new URL('index-68bf89fa8abfd4f1_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -548,8 +548,8 @@ async function init(input) {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper303 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 91, __wbg_adapter_24);
+    imports.wbg.__wbindgen_closure_wrapper306 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 92, __wbg_adapter_24);
         return addHeapObject(ret);
     };
 
